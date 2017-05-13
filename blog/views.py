1770c6b2508f82d4439786843cdc743a1827f471
TildaStrand <lehrer.m.j@gmail.com>
2017-05-13 17:57:43 +0200

    trash added

--- blog/views.py
+++ blog/views.py
@@ -39,3 +39,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk).delete()
+    return redirect ('post_list')
